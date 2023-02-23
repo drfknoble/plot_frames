9301cf635f78b3623884ddb8545eb0e8bc120509
DrFKNoble <f.k.noble@massey.ac.nz>
2023-02-23 15:52:09 +1300

    Minor tweak.

--- src/plot_frames/__init__.py
+++ src/plot_frames/__init__.py
@@ -5,8 +5,10 @@ __all__ = [
     "plot_text2d",
     "plot_point2d",
     "plot_arrow2d",
+    "plot_rot2d",
     "plot_pose2d",
     "plot_text3d",
     "plot_point3d",
     "plot_arrow3d",
+    "plot_rot3d",
     "plot_pose3d"]
